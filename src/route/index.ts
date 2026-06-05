341cfd796236bd7d67aeaecf67c96c2fd111a8c8
LYBF <1438071241@qq.com>
2026-06-05 11:44:46 +0800

    fix

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createMemoryHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
     {
@@ -30,7 +30,7 @@ const routes = [
     }
 ]
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
